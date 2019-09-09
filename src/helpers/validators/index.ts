bfb4528938e468cedf2df550ce0d09c30a0017c3
Tymofieiev Pavlo <pavlo.tymofieiev@betlab.com>
2019-09-09 11:38:25 +0300

    ESLint: fix

--- src/helpers/validators/index.ts
+++ src/helpers/validators/index.ts
@@ -4,7 +4,8 @@ export const required: TValidator = (value) => (value ? undefined : 'Required');
 
 const CAR_NUMBER_REG_EXP = /^[A-ZА-Я]{2}[0-9]{4}[A-ZА-Я]{2}$/;
 
-export const carNumber: TValidator = value => (CAR_NUMBER_REG_EXP.test(value) ? undefined : 'Not a car number');
+export const carNumber: TValidator = value =>
+  (CAR_NUMBER_REG_EXP.test(value) ? undefined : 'Not a car number');
 
 export const composeValidators = (...validators: Array<TValidator>) => (value: TValue): TError =>
   validators.reduce((error, validator) => error || validator(value), undefined) as TError;
